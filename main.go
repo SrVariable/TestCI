af46f24fcca57774e9994e06434f4ede949b0e75
SrVariable <rojohnibana@gmail.com>
2025-04-11 04:58:52 +0200

    Mess the format of main.go

--- main.go
+++ main.go
@@ -17,6 +17,6 @@ func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7}
 
 	fmt.Println(add(nums...))
-	fmt.Println(add(nums[3:]...))
+	    fmt.Println(add(nums[3:]...))
 	fmt.Println(add(nums[:3]...))
 }
